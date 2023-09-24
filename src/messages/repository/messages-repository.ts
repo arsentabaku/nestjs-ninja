c7c0f1978acc7ad41de21fe4aabf08a108292423
Arsen Tabaku <arsen.tabaku@vrdirect.com>
2023-09-24 12:05:31 +0200

    Implementing the messages repository

---
+++ src/messages/repository/messages-repository.ts
@@ -0,0 +1,28 @@
+import { readFile, writeFile } from 'fs/promises';
+
+export class MessagesRepository {
+  async findOne(id: string) {
+    const content = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(content);
+
+    return messages[id];
+  }
+
+  async findAll() {
+    const content = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(content);
+
+    return messages;
+  }
+
+  async create(messageContent: string) {
+    const content = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(content);
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, messageContent };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
+}
